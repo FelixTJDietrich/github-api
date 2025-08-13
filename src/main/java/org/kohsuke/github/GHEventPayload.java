8128c2cd0f3141f7a78e052e9bb37f8f46883d91
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-08-13 15:08:14 +0200

    format

--- src/main/java/org/kohsuke/github/GHEventPayload.java
+++ src/main/java/org/kohsuke/github/GHEventPayload.java
@@ -999,6 +999,32 @@ public abstract class GHEventPayload extends GitHubInteractiveObject {
     }
 
     /**
+     * A milestone event was triggered.
+     *
+     * @see <a href="https://docs.github.com/en/webhooks/webhook-events-and-payloads#milestone">milestone event</a>
+     */
+    public static class Milestone extends GHEventPayload {
+
+        private GHMilestone milestone;
+
+        /**
+         * Create default Milestone instance
+         */
+        public Milestone() {
+        }
+
+        /**
+         * Gets the milestone.
+         *
+         * @return the milestone
+         */
+        @SuppressFBWarnings(value = { "EI_EXPOSE_REP" }, justification = "Expected")
+        public GHMilestone getMilestone() {
+            return milestone;
+        }
+    }
+
+    /**
      * A ping.
      *
      * <a href="https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#ping"> ping
@@ -1972,32 +1998,6 @@ public abstract class GHEventPayload extends GitHubInteractiveObject {
         }
     }
 
-    /**
-     * A milestone event was triggered.
-     *
-     * @see <a href="https://docs.github.com/en/webhooks/webhook-events-and-payloads#milestone">milestone event</a>
-     */
-    public static class Milestone extends GHEventPayload {
-
-        /**
-         * Create default Milestone instance
-         */
-        public Milestone() {
-        }
-
-        private GHMilestone milestone;
-
-        /**
-         * Gets the milestone.
-         *
-         * @return the milestone
-         */
-        @SuppressFBWarnings(value = { "EI_EXPOSE_REP" }, justification = "Expected")
-        public GHMilestone getMilestone() {
-            return milestone;
-        }
-    }
- 
     // https://docs.github.com/en/free-pro-team@latest/developers/webhooks-and-events/webhook-events-and-payloads#webhook-payload-object-common-properties
     // Webhook payload object common properties: action, sender, repository, organization, installation
     private String action;
